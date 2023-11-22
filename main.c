8d75ab76efde95e3d72a51f815eab3e013910816
Julius Jänchen <julius.jaenchen@check24.de>
2023-11-22 12:02:10 +0100

    simplified main

--- main.c
+++ main.c
@@ -7,25 +7,14 @@ int main(int argc, char const *argv[])
     char* patterns[PATTERN_COUNT] = {"berlin", "dresden", "fokus", "wiesbaden"};
     pthread_t threads[PATTERN_COUNT];
 
-    
-
     for (int i = 0; i < PATTERN_COUNT; i++) {
         printf("started thread %d (searching for '%s')\n", i, patterns[i]);
-        char* patternPtr = (char*) malloc(sizeof(patterns[i]));
-        // TODO: free() dataPtr
-        if (patternPtr == NULL) {
-            printf("dataPtr allocation failed");
-            exit(1);
-        }
-        strcpy(patternPtr, patterns[i]);
-        pthread_create(&threads[i], NULL, ThrdFunc, patternPtr);
-        // searchForPatternInFile(patternPtr);
+        pthread_create(&threads[i], NULL, ThrdFunc, patterns[i]);
     }
 
     for (int i = 0; i < PATTERN_COUNT; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    // TODO: loop to wait for threads
     exit(0);
 }
