29c38eff1669453f81a6c23612f3bfe79f162bcf
Julius Jänchen <julius.jaenchen@check24.de>
2023-11-24 10:00:46 +0100

    removed early return statement

--- main.c
+++ main.c
@@ -6,15 +6,25 @@ int main(int argc, char const *argv[])
 {
     char* patterns[PATTERN_COUNT] = {"berlin", "dresden", "fokus", "wiesbaden"};
     pthread_t threads[PATTERN_COUNT];
+    int exitStatus = 0;
 
     for (int i = 0; i < PATTERN_COUNT; i++) {
         printf("started thread %d (searching for '%s')\n", i, patterns[i]);
         pthread_create(&threads[i], NULL, ThrdFunc, patterns[i]);
     }
 
-    for (int i = 0; i < PATTERN_COUNT; i++) {
-        pthread_join(threads[i], NULL);
+    for (int i = 0; i < PATTERN_COUNT && exitStatus == 0; i++) {
+        char* errorMessage = NULL;
+        void* errorMessagePtr = &errorMessage;
+        
+        pthread_join(threads[i], errorMessagePtr);
+
+        if (errorMessage != NULL) {
+            exitStatus = 1;
+            printf("ERROR: %s", errorMessage);
+            free(errorMessage);
+        }
     }
 
-    exit(0);
+    return exitStatus;
 }
--- threadfunc.c
+++ threadfunc.c
@@ -1,46 +1,39 @@
 #include "threadfunc.h"
 
 
-char getPatternChar(char* patternPtr, int offset) {
-    return *(patternPtr + offset);
-}
-
 void* ThrdFunc(void* arg) {
     clock_t startTime = clock();
-    
+    char* errorMessage = NULL;
     char* pattern = (char*)arg;
-    int patternLength;
-    for (int i = 0; pattern[i] != '\0'; i++) {
-        patternLength = i;
-    }
+    int patternLength = strlen(pattern);
 
     FILE* fileDescriptor = fopen(FILENAME, "r");
     if (fileDescriptor == NULL) {
-        printf("Failed to open file '%s'\n", FILENAME);
-        exit(1);
-    }
-
-    bool wasPatternFound = false;
-    char lineBuffer[LINE_BUFFER_SIZE];
-    while (!wasPatternFound && fgets(lineBuffer, LINE_BUFFER_SIZE, fileDescriptor) != NULL) {
-        bool shouldContinue = false;
-        for (int i = 0; i < patternLength && !shouldContinue; i++) {
-            if (pattern[i] != lineBuffer[i]) {
-                shouldContinue = true;
+        errorMessage = calloc(30 + strlen(FILENAME), sizeof(char));
+        sprintf(errorMessage, "failed to open file '%s'\n", FILENAME);
+    } else {
+        bool wasPatternFound = false;
+        char lineBuffer[LINE_BUFFER_SIZE];
+        while (!wasPatternFound && fgets(lineBuffer, LINE_BUFFER_SIZE, fileDescriptor) != NULL) {
+            bool shouldContinue = false;
+            for (int i = 0; i < patternLength && !shouldContinue; i++) {
+                if (pattern[i] != lineBuffer[i]) {
+                    shouldContinue = true;
+                }
+            }
+            if (!shouldContinue) {
+                wasPatternFound = true;
             }
         }
-        if (!shouldContinue) {
-            wasPatternFound = true;
+            
+        double elapsedTime = (double)(clock() - startTime) / CLOCKS_PER_SEC;
+        if (wasPatternFound) {
+            printf("Pattern '%s' was found in %s. Took %.5fs\n", pattern, FILENAME, elapsedTime);
+        } else {
+            printf("Pattern '%s' was not found in %s. Took %.5fs\n", pattern, FILENAME, elapsedTime);
         }
     }
-        
-    double elapsedTime = (double)(clock() - startTime) / CLOCKS_PER_SEC;
-    if (wasPatternFound) {
-        printf("Pattern '%s' was found in %s. Took %.5fs\n", pattern, FILENAME, elapsedTime);
-    } else {
-        printf("Pattern '%s' was not found in %s. Took %.5fs\n", pattern, FILENAME, elapsedTime);
-    }
-
+    
     fclose(fileDescriptor);
-    return NULL;
+    return errorMessage;
 }
